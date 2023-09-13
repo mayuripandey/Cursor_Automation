3a240d8a47c7ae4c9db46c6e6388065f80113446
Mayuri Pandey <mayuripandey4@gmail.com>
2023-09-13 13:50:57 +0200

    added fail safe for corner issues

--- Cursor_with coordinates.py
+++ Cursor_with coordinates.py
@@ -2,9 +2,11 @@ import pyautogui as pag
 import time
 import random
 
+pag.FAILSAFE= False
+
 
 while True:
     x = random.randint(600, 700)
     y = random.randint(200,600)
     pag.moveTo(x,y,0.5)
-    time.sleep(2)
+    time.sleep(4)
