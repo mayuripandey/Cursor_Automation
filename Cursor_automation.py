7f470cae0fad2b532c3bdc54040ff401f3a54f8a
Mayuri Pandey <mayuripandey4@gmail.com>
2023-09-13 09:51:20 +0200

    Curson automation by two ways

---
+++ Cursor_automation.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+while True:
+    pyautogui.click(100,100)
+    time.sleep(3)
+    pyautogui.click(200, 200)
+    time.sleep(6)
+
+
+print('Hey bye')
---
+++ Cursor_with coordinates.py
@@ -0,0 +1,10 @@
+import pyautogui as pag
+import time
+import random
+
+
+while True:
+    x = random.randint(600, 700)
+    y = random.randint(200,600)
+    pag.moveTo(x,y,0.5)
+    time.sleep(2)
